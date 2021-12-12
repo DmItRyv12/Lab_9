96d54493c3dea4df1cbc83481ba9197e5391caf6
Дмитрий Минаков <m1nak0vd@yandex.ru>
2021-12-12 12:27:41 +0300

    Добавьте файлы проекта.

---
+++ Lab_9/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lab_9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            try
+            {
+                Console.WriteLine(a / b);
+            }
+            // catch(Exception ex)
+            // catch(DivideByZeroException)
+            catch (DivideByZeroException) when (a == 0 && b == 0)
+            {
+                // Console.WriteLine("Ошибка!");
+                // Console.WriteLine(ex.Message);
+                // Console.WriteLine("Деление на 0!");
+                Console.WriteLine("Деление 0 на 0!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
